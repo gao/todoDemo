3bc2d7316c819427ea0722195b423703b00aa247
GaoBin <bin_gao@live.cn>
2012-11-06 09:47:33 +0800

    first commit set up the app

---
+++ src/main/webapp/js/4_app.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+(function($){
+	//Remote URL
+	app.remoteServiceURL = "http://localhost:8080/todo";
+	
+	// if mode is 'SQLite' use SQLiteDao,
+	// if mode is 'Remote' use remote dao
+	// else use memory dao
+	app.dataMode = 'Remote';
+	
+	// if support touchstart event use touchstart, else use mousedown
+	app.pressEvent = brite.ua.hasTouch() ? "touchstart" : "mousedown";
+})(jQuery);
+
